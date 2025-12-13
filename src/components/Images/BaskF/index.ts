ed18ce7b72b995ac82d861ded90ff399f48806a2
Chino <chinomsoaugustine073@gmail.com>
2025-12-12 23:09:46 -0800

    Add BaskF images required for Basketball component

---
+++ src/components/Images/BaskF/index.ts
@@ -0,0 +1,49 @@
+
+
+//Yaw
+import B1 from "./B1.JPG"
+import B3 from "./B24.JPG"
+import B2 from "./B2.JPG";
+import B4 from "./B3.JPG";
+import B5 from "./B4.JPG";
+import B6 from "./B5.JPG";
+
+
+import B8 from "./B7.JPG";
+import B9 from "./B8.JPG";
+import B10 from "./B9.JPG";
+import B11 from "./B10.JPG";
+import B12 from "./B11.JPG";
+import B13 from "./B12.JPG";
+
+
+import B14 from "./B13.JPG"
+import B15 from "./B14.JPG"
+import B16 from "./B15.JPG"
+import B17 from "./B16.JPG"
+
+import B18 from "./B17.JPG"
+import B19 from "./B18.JPG"
+import B20 from "./B19.JPG"
+import B21 from "./B20.JPG";
+import B22 from "./B21.JPG";
+import B23 from "./B22.JPG";
+import B24 from "./B23.JPG";
+
+import B25 from "./B25.JPG"
+import B26 from "./B26.JPG"
+import B27 from "./B27.JPG"
+import B28 from "./B28.JPG"
+
+
+export const BK = [
+    B1, B2, B3,
+    B15, B9, B23,
+    B8, B6, B10,
+    B11, B12, B13,
+    B14, B16, B4,
+    B17, B18, B19,
+    B20, B21, B22,
+    B24, B5, B25,
+    B26, B27, B28,
+]
