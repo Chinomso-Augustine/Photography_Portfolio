8f05814f441ad975848f6e5a5c6a3cca2912cfe8
Chino <chinomsoaugustine073@gmail.com>
2025-12-12 23:12:04 -0800

    Add FootBall images required for Football component

---
+++ src/components/Images/FootBall/index.ts
@@ -0,0 +1,41 @@
+
+
+//Yaw
+import Bk1 from "./Bk1.JPG";
+import Bk2 from "./Bk24.JPG"
+import Bk3 from "./Bk2.JPG";
+import Bk4 from "./Bk3.JPG";
+import Bk5 from "./Bk4.JPG";
+import Bk6 from "./Bk5.JPG";
+
+
+import Bk8 from "./Bk7.JPG";
+import Bk9 from "./Bk8.JPG";
+import Bk10 from "./Bk9.JPG";
+import Bk11 from "./Bk10.JPG";
+import Bk13 from "./Bk12.JPG";
+
+
+import Bk14 from "./Bk13.JPG"
+import Bk15 from "./Bk14.JPG"
+import Bk16 from "./Bk15.JPG"
+import Bk17 from "./Bk16.JPG"
+
+import Bk18 from "./Bk17.JPG"
+import Bk19 from "./Bk18.JPG"
+import Bk21 from "./Bk20.JPG";
+import Bk23 from "./Bk22.JPG";
+import Bk24 from "./Bk23.JPG";
+import Bk25 from "./Bk23.JPG";
+
+
+export const FArray = [
+    Bk23, Bk24, Bk25,
+    Bk1, Bk2, Bk4,
+    Bk5, Bk6, Bk3,
+    Bk11, Bk8, Bk9,
+    Bk10, Bk13, Bk19,
+    Bk14, Bk15, Bk16,
+    Bk17, Bk18, Bk21,
+
+]
