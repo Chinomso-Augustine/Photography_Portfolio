4ff19fba12e5a85f15ac2fbf3b7d6293061f715a
Chino <chinomsoaugustine073@gmail.com>
2025-12-12 23:05:34 -0800

    Add ProfF images required for Professional component

---
+++ src/components/Images/ProfF/index.ts
@@ -0,0 +1,86 @@
+
+
+//Yaw
+import P1 from "./P1.JPG";
+import P2 from "./P2.JPG";
+import P4 from "./P3.JPG";
+import P5 from "./P4.JPG";
+import P6 from "./P5.JPG";
+
+
+import P8 from "./P7.JPG";
+import P9 from "./P8.JPG";
+import P10 from "./P9.JPG";
+import P11 from "./P10.JPG";
+import P12 from "./P11.JPG";
+import P13 from "./P12.JPG";
+
+
+import P14 from "./P13.JPG"
+import P15 from "./P14.JPG"
+import P16 from "./P15.JPG"
+import P17 from "./P16.JPG"
+
+import P18 from "./P17.JPG"
+import P20 from "./P19.JPG"
+import P21 from "./P20.JPG";
+import P22 from "./P21.JPG";
+import P23 from "./P22.JPG";
+import P24 from "./P23.JPG";
+
+
+import P25 from "./P24.JPG";
+import P26 from "./P25.JPG"
+import P27 from "./P26.JPG";
+import P28 from "./P27.JPG";
+import P29 from "./P28.JPG";
+import P30 from "./P29.JPG";
+
+
+import P31 from "./P30.JPG";
+import P32 from "./P31.JPG";
+import P33 from "./P32.JPG";
+import P34 from "./P33.JPG";
+import P35 from "./P34.JPG";
+import P36 from "./P35.JPG";
+import P37 from "./P36.JPG";
+
+
+import P38 from "./P37.JPG"
+import P39 from "./P38.JPG"
+import P40 from "./P39.JPG"
+import P41 from "./P40.JPG"
+
+import P42 from "./P41.JPG"
+import P43 from "./P42.JPG"
+import P44 from "./P43.JPG"
+import P45 from "./P44.JPG";
+import P46 from "./P45.JPG";
+import P47 from "./P46.JPG";
+import P48 from "./P47.JPG";
+import P49 from "./P48.JPG";
+import P50 from "./P49.JPG";
+import P51 from "./P50.JPG";
+
+
+export const ProfArray = [
+    P1, P2, P51,
+    P4, P5, P6,
+    P8, P9, P10,
+    P39, P40, P13,
+    P11, P12, P41,
+    P18, P15, P16,
+    P22, P23, P20,
+
+
+    P24, P21, P26,
+    P27, P28, P29,
+    P30, P31, P32,
+
+    P33, P37, P34,
+    P36, P25, P44,
+    P17, P35, P14,
+    P42, P43, P38,
+    P45, P46, P47,
+    P48, P49, P50,
+]
