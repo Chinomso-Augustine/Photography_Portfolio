fbb229d179a2a4b99a417bdded1e02f2b23e5bc9
Chino <chinomsoaugustine073@gmail.com>
2025-12-12 23:18:10 -0800

    Add SoccF images required for Soccer component

---
+++ src/components/Images/SoccF/index.ts
@@ -0,0 +1,126 @@
+
+import soc1 from "./Soccer1.JPG"
+import soc2 from "./Soccer2.JPG"
+import soc3 from "./Soc3.JPG"
+import soc4 from "./Soc4.JPG"
+
+import S1 from "./S1.JPG";
+import S2 from "./S2.JPG";
+import S3 from "./S3.JPG";
+import S4 from "./S4.JPG";
+import S5 from "./S5.JPG";
+import S6 from "./S6.JPG";
+import S7 from "./S7.JPG";
+import S8 from "./S8.JPG";
+import S9 from "./S9.JPG";
+import S10 from "./S10.JPG";
+import S11 from "./S11.JPG";
+import S12 from "./S12.JPG";
+import S13 from "./S13.JPG";
+import S14 from "./S14.JPG";
+import S15 from "./S15.JPG";
+import S17 from "./S17.JPG";
+import S18 from "./S18.JPG";
+import S19 from "./S19.JPG";
+import S20 from "./S20.JPG";
+import S21 from "./S21.JPG";
+import S22 from "./S22.JPG";
+import S23 from "./S23.JPG";
+import S24 from "./S24.JPG";
+import S25 from "./S25.JPG";
+import S26 from "./S26.JPG";
+import S27 from "./S27.JPG";
+import S29 from "./S29.JPG";
+import S30 from "./S30.JPG";
+import S31 from "./S31.JPG";
+import S32 from "./S32.JPG";
+import S33 from "./S33.JPG";
+import S34 from "./S34.JPG";
+import S35 from "./S35.JPG";
+import S36 from "./S36.JPG";
+import S37 from "./S37.JPG";
+import S39 from "./S39.JPG";
+import S40 from "./S40.JPG";
+import S41 from "./S41.JPG";
+import S42 from "./S42.JPG";
+import S43 from "./S43.JPG";
+import S44 from "./S44.JPG";
+import S45 from "./S45.JPG";
+import S46 from "./S46.JPG";
+
+import S47 from "./S47.JPG";
+import S48 from "./S48.JPG";
+import S49 from "./S49.JPG";
+import S50 from "./S50.JPG";
+import S51 from "./S51.JPG";
+import S52 from "./S52.JPG";
+import S53 from "./S53.JPG";
+import S54 from "./S54.JPG";
+import S55 from "./S55.JPG";
+import S56 from "./S56.JPG";
+import S57 from "./S57.JPG";
+import S58 from "./S58.JPG";
+
+
+import S59 from "./S59.JPG";
+import S60 from "./S60.JPG";
+import S61 from "./S61.JPG";
+import S62 from "./S62.JPG";
+import S63 from "./S63.JPG";
+
+import W1 from "./W1.JPG"
+import W2 from "./W2.JPG"
+import W3 from "./W3.JPG"
+import W4 from "./W4.JPG"
+import W5 from "./W5.JPG"
+import W6 from "./W6.JPG"
+import W8 from "./W8.JPG"
+import W10 from "./W10.JPG"
+import W11 from "./W11.JPG"
+import W12 from "./W12.JPG"
+import W13 from "./W13.JPG"
+import W14 from "./W14.JPG"
+import W15 from "./W15.JPG"
+import W16 from "./W16.JPG"
+import W17 from "./W17.JPG"
+import W18 from "./W18.JPG"
+import W19 from "./W19.JPG"
+import W23 from "./W23.JPG"
+import W24 from "./W24.JPG"
+import W25 from "./W25.JPG"
+import W26 from "./W26.JPG"
+import W27 from "./W27.JPG"
+import W28 from "./W28.JPG"
+import W29 from "./W29.JPG"
+import W30 from "./W30.JPG"
+import W31 from "./W31.JPG"
+
+//Women 
+
+export const soccerImg =
+  [
+    soc1, soc2, S4, W28,
+    soc3, soc4, S3, S5,
+    S1, S6, W23, S7,
+    W10, W8, S10, S11,
+    S20, W12, W19, S25,
+    S15, S24, S21, S22,
+    S40, S30, S39, S31,
+    S27, S29, S32, S33,
+    S36, S37, S34, S41,
+    S45, S46, S35, S44,
+    S23, S19, S51, S47,
+    S49, S50, S42, S52,
+    S57, S54, S55, S56,
+    S53, S62, S63, S60,
+    S61, S58, S59, S2,
+    W31, S43, S48, W1,
+    W2, W3, W4, W5,
+    W11, W26, S14, S18,
+
+    W13, W14, S13, S9,
+    W15, W16, W6, W18,
+    W27, W24, S12, W25,
+    S17, W29, S8, W30,
+
+  ];
