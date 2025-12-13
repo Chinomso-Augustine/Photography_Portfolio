2dc3b5b2be2720b71a161e3a3f8e85ed6832e2d1
Chino <chinomsoaugustine073@gmail.com>
2025-12-12 23:02:21 -0800

    Add GradF images required for Graduation component

---
+++ src/components/Images/GradF/index.ts
@@ -0,0 +1,50 @@
+
+
+//Yaw
+import A1 from "./Yaw1.JPEG";
+import A2 from "./Yaw2.JPEG";
+import A4 from "./Yaw3.JPEG";
+import A5 from "./Yaw5.JPEG";
+import A6 from "./Yaw6.JPEG";
+
+
+import A7 from "./Osh1.JPG";
+import A8 from "./Osh9.JPG";
+import A9 from "./Grad4.JPG"
+import A10 from "./Grad5.JPG";
+import A11 from "./Grad8.JPEG";
+import A12 from "./Grad9.JPEG";
+import A13 from "./Grad10.JPEG";
+
+
+import A14 from "./Osh2.JPEG";
+import A15 from "./Osh3.JPEG";
+import A16 from "./Osh4.JPEG";
+import A17 from "./Osh5.JPEG";
+
+import A18 from "./Osh6.JPEG";
+import A19 from "./Osh7.JPEG";
+import A20 from "./Osh8.JPEG";
+import A21 from "./Grad25.JPEG";
+import A22 from "./Grad26.JPEG";
+import A23 from "./Grad27.JPEG";
+
+
+import A25 from "./Wal2.JPG";
+import A26 from "./Wal3.JPG";
+import A27 from "./Wal4.JPEG";
+import A28 from "./Wal5.JPG";
+import A29 from "./Wal6.JPG";
+
+export const GradArray = [
+    A1, A2, A4, 
+     A27,A28, A25,
+    A5, A6, A10,
+    A11, A12, A13, 
+    A14, A15, A16, 
+    A17, A18, A19, 
+    A20, A21, A22, 
+    A23, A9, A29,
+   
+      A26, A8,
+]
