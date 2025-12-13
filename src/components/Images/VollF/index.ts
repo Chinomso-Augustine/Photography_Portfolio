fa4a7044cb4384c32956149ef931fc2a61098bc9
Chino <chinomsoaugustine073@gmail.com>
2025-12-12 23:35:35 -0800

    Add VollF images required for Volleyball component

---
+++ src/components/Images/VollF/index.ts
@@ -0,0 +1,86 @@
+
+
+//Yaw
+import V1 from "./V1.JPG"
+import V3 from "./V3.JPG"
+import V2 from "./V2.JPG"
+import V4 from "./V4.JPG"
+import V5 from "./V5.JPG"
+import V6 from "./V6.JPG"
+
+
+import V7 from "./V7.JPG"
+import V8 from "./V8.JPG"
+import V9 from "./V9.JPG"
+import V10 from "./V10.JPG"
+import V11 from "./V11.JPG"
+import V12 from "./V12.JPG"
+import V13 from "./V13.JPG"
+
+
+import V14 from "./V14.JPG"
+import V15 from "./V15.JPG"
+import V16 from "./V16.JPG"
+import V17 from "./V17.JPG"
+
+import V18 from "./V18.JPG"
+import V19 from "./V19.JPG"
+import V20 from "./V20.JPG"
+import V21 from "./V21.JPG"
+import V22 from "./V22.JPG"
+import V23 from "./V23.JPG"
+import V24 from "./V24.JPG"
+
+import V25 from "./V25.JPG"
+import V26 from "./V26.JPG"
+import V27 from "./V27.JPG"
+import V28 from "./V28.JPG"
+import V29 from "./V29.JPG"
+
+
+import V30 from "./V30.JPG"
+import V31 from "./V31.JPG"
+import V32 from "./V32.JPG"
+import V33 from "./V33.JPG"
+import V34 from "./V34.JPG"
+import V35 from "./V35.JPG"
+import V36 from "./V36.JPG"
+
+
+import V37 from "./V37.JPG"
+import V39 from "./V39.JPG"
+import V40 from "./V40.JPG"
+
+import V41 from "./V41.JPG"
+import V42 from "./V42.JPG"
+import V43 from "./V43.JPG"
+import V44 from "./V44.JPG"
+import V46 from "./V46.JPG"
+import V47 from "./V47.JPG"
+import V48 from "./V48.JPG"
+import V49 from "./V49.JPG"
+
+
+
+
+
+export const VArray = [
+    V47, V2, V5,
+    V4, V6, V3,
+    V7, V9, V12,
+    V10, V15, V8,
+
+    V13, V14, V16,
+    V49, V26, V11,
+    V19, V20, V21,
+    V22, V23, V24,
+
+    V25, V35, V46,
+    V28, V29, V30,
+    V31, V32, V33,
+    V34, V27, V39,
+    V37, V40, V42,
+    V41, V18, V1,
+    V44, V17, V43,
+
+]
